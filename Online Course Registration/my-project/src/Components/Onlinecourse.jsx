b9b7a61201f37923ec85151731219b404ab79b13
Mr. Jay Patel <jayvekariya2003@gmail.com>
2024-04-06 17:53:25 +0530

    tesing

--- Online Course Registration/my-project/src/Components/Onlinecourse.jsx
+++ Online Course Registration/my-project/src/Components/Onlinecourse.jsx
@@ -26,7 +26,7 @@ const Onlinecourse = () => {
                 <br />
                 <hr />
                 <br />
-                <button id="enroll">Enroll Now</button>
+                <button id="enroll">Enroll Now.!</button>
               </div>
             </div>
           </div>
